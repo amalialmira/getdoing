6fa788f785bf110700e20cc64a0f90347964d463
amalialmira <amaliaalmira16@gmail.com>
2024-10-27 16:39:34 +0700

    section baca juga

---
+++ getgoing/src/components/Card3.jsx
@@ -0,0 +1,158 @@
+const Card3 = () => {
+    return (
+        <div style={{ fontFamily: "Poppins" }} className="py-4 mx-auto lg:max-w-7xl sm:max-w-full">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-3 gap-6">
+                <div className="bg-gray-50 shadow-[0_0_20px_rgba(128,128,128,0.3)] overflow-hidden rounded-lg cursor-pointer hover:-translate-y-2 transition-all relative">
+                    <div className="overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+                        <img
+                            src="https://ik.imagekit.io/getgoing/tr:w-1000,h-1000/v0/b/getgoing-prod.appspot.com/o/tripPlanner%2F17162716641.png?alt=media&token=b9a07845-28b3-4be6-adab-acbc4c4c0ce4"
+                            alt="Product 1"
+                            className="h-full w-full object-contain"
+                        />
+                    </div>
+                    <div className="p-6 bg-white">
+                        <a href="https://getgoing.co.id/blog/61d8284b-3a91-4b2b-ad9d-caf6c11dfba5" className="text-xl font-bold text-gray-800 hover:underline mb-10">
+                            Wisata Menarik di Sungai Thames Jelajahi Keindahan dan Sejarah London
+                        </a>
+                        <div
+                            href="javascript:void(0)"
+                            className="text-gray-400 text-sm flex items-center mt-5"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                className="w-[15px] h-[15px] mr-2"
+                                viewBox="0 0 510 510"
+                            >
+                                <g fillOpacity="1">
+                                    <path
+                                        d="M255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
+                                        data-original="#000000"
+                                    />
+                                    <path
+                                        d="M267.75 127.5H229.5v153l132.6 81.6 20.4-33.15-114.75-68.85z"
+                                        data-original="#000000"
+                                    />
+                                </g>
+                            </svg>
+                            <span>21 Mei 2024</span>
+                        </div>
+                        <a href="https://getgoing.co.id/blog/SeruSeruanJelajahiKotadiEropaEksplorasiCityTouryangWajibDicoba" className="flex items-center mt-5 text-red-600 hover:underline hover:text-red-700">
+                            <p className="text-sm font-bold mr-2">LANJUTKAN MEMBACA</p>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="16px"
+                                className="fill-white"
+                            >
+                                <path d="M4 12h12" stroke="currentColor" strokeWidth="3" strokeLinecap="round" />
+                                <path d="M16 6l6 6-6 6z" fill="currentColor" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <div className="bg-gray-50 shadow-[0_0_20px_rgba(128,128,128,0.3)] overflow-hidden rounded-lg cursor-pointer hover:-translate-y-2 transition-all relative">
+                    <div className="overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+                        <img
+                            src="https://ik.imagekit.io/getgoing/tr:w-1000,h-1000/v0/b/getgoing-prod.appspot.com/o/blogImage%2Ff7d81d84-52b6-4748-a4c4-38182274abac?alt=media&token=354ca680-8a53-4d7d-a405-a02c6ef53d74"
+                            alt="Product 2"
+                            className="h-full w-full object-contain"
+                        />
+                    </div>
+                    <div className="p-6 bg-white">
+                        <a href="https://getgoing.co.id/blog/61d8284b-3a91-4b2b-ad9d-caf6c11dfba5" className="text-xl font-bold text-gray-800 hover:underline mb-10">
+                            Seru-Seruan Jelajahi Kota di Eropa Eksplorasi City Tour yang Wajib Dicoba!
+                        </a>
+                        <div
+                            href="javascript:void(0)"
+                            className="text-gray-400 text-sm flex items-center mt-5"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                className="w-[15px] h-[15px] mr-2"
+                                viewBox="0 0 510 510"
+                            >
+                                <g fillOpacity="1">
+                                    <path
+                                        d="M255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
+                                        data-original="#000000"
+                                    />
+                                    <path
+                                        d="M267.75 127.5H229.5v153l132.6 81.6 20.4-33.15-114.75-68.85z"
+                                        data-original="#000000"
+                                    />
+                                </g>
+                            </svg>
+                            <span>2 Jun 2023</span>
+                        </div>
+                        <a href="https://getgoing.co.id/blog/SeruSeruanJelajahiKotadiEropaEksplorasiCityTouryangWajibDicoba" className="flex items-center mt-5 text-red-600 hover:underline hover:text-red-700">
+                            <p className="text-sm font-bold mr-2">LANJUTKAN MEMBACA</p>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="16px"
+                                className="fill-white"
+                            >
+                                <path d="M4 12h12" stroke="currentColor" strokeWidth="3" strokeLinecap="round" />
+                                <path d="M16 6l6 6-6 6z" fill="currentColor" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <div className="bg-gray-50 shadow-[0_0_20px_rgba(128,128,128,0.3)] overflow-hidden rounded-lg cursor-pointer hover:-translate-y-2 transition-all relative">
+                    <div className="overflow-hidden mx-auto aspect-w-16 aspect-h-8">
+                        <img
+                            src="https://ik.imagekit.io/getgoing/tr:w-1000,h-1000/v0/b/getgoing-prod.appspot.com/o/blogImage%2F426aa324-48f2-4927-b06f-f8fd0728ed16?alt=media&token=e3e7118e-70f2-47e9-9ffc-e7e85a8ad4fe"
+                            alt="Product 3"
+                            className="h-full w-full object-contain"
+                        />
+                    </div>
+                    <div className="p-6 bg-white">
+                        <a href="https://getgoing.co.id/blog/61d8284b-3a91-4b2b-ad9d-caf6c11dfba5" className="text-xl font-bold text-gray-800 hover:underline mb-10">
+                            Daylight Saving Time Upaya Maksimalkan Cahaya Matahari dan Penghematan Energi
+                        </a>
+                        <div
+                            href="javascript:void(0)"
+                            className="text-gray-400 text-sm flex items-center mt-5"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                className="w-[15px] h-[15px] mr-2"
+                                viewBox="0 0 510 510"
+                            >
+                                <g fillOpacity="1">
+                                    <path
+                                        d="M255 0C114.75 0 0 114.75 0 255s114.75 255 255 255 255-114.75 255-255S395.25 0 255 0zm0 459c-112.2 0-204-91.8-204-204S142.8 51 255 51s204 91.8 204 204-91.8 204-204 204z"
+                                        data-original="#000000"
+                                    />
+                                    <path
+                                        d="M267.75 127.5H229.5v153l132.6 81.6 20.4-33.15-114.75-68.85z"
+                                        data-original="#000000"
+                                    />
+                                </g>
+                            </svg>
+                            <span>27 Okt 2023</span>
+                        </div>
+                        <a href="https://getgoing.co.id/blog/DaylightSavingTimeUpayaMaksimalkanCahayaMataharidanPenghematanEnergi" className="flex items-center mt-5 text-red-600 hover:underline hover:text-red-700">
+                            <p className="text-sm font-bold mr-2">LANJUTKAN MEMBACA</p>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="16px"
+                                className="fill-white"
+                            >
+                                <path d="M4 12h12" stroke="currentColor" strokeWidth="3" strokeLinecap="round" />
+                                <path d="M16 6l6 6-6 6z" fill="currentColor" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Card3;
